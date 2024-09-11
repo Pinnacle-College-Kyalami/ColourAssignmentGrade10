fc891d42b62ec36f11fffa5fcc184e8add3efe56
TeacherRenier <relindeque@pinnaclecolleges.co.za>
2024-09-11 17:37:03 +0200

    Update ColoutTest.java

--- ColoutTest.java
+++ ColoutTest.java
@@ -1,1 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ColourTest {
+
+    @Test
+    public void testColourChange() {
+        ColourAssignment assignment = new ColourAssignment();
+        
+        for (int i = 0; i <= 255; i++) {
+            int red = 255 - i;
+            int blue = 0 + i;
+            assertEquals(red, assignment.getRedValue(i));
+            assertEquals(blue, assignment.getBlueValue(i));
+        }
+    }
+}
 
